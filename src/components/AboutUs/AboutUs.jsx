e7763ef71d5d499b8a897b6335f949247714e27e
Dom Brown <dominicbrown4@hotmail.com>
2025-03-28 11:23:09 +0000

    edited styling for components

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -6,8 +6,10 @@ function AboutUs() {
     <section className="about-us">
       <h2 className="about-heading">About Simpli.Fly</h2>
       <p className="about-text">
-        Simpli.Fly is an easy-to-use inventory management system built for a range of aviation cargo sizes. 
-        Whether you're transporting your personal belongings or managing a high-capacity fleet of aircraft, Simpli.Fly will help you track your inventory quickly, efficiently, and accurately.
+        Simpli.Fly is an easy-to-use inventory management system that aims to simplify the aircraft loading process for a range of aviation cargo sizes. 
+      </p>
+      <p className="about-text">
+        Whether you're transporting your personal belongings in a single engine airplane or managing a high-capacity fleet of aircraft, Simpli.Fly will help you track your inventory quickly, efficiently, and accurately.
       </p>
     </section>
   );
--- src/components/GettingStarted/GettingStarted.jsx
+++ src/components/GettingStarted/GettingStarted.jsx
@@ -14,7 +14,12 @@ function GettingStarted() {
         <li>Select your cargo size and get going!</li>
       </ol>
       <div className="d-flex justify-content-center gap-3 mt-4">
-        <a href="#cargo" className="btn btn-primary btn-lg custom-btn">Register</a>
+  <a href="#cargo" className="btn btn-primary btn-lg custom-btn d-flex align-items-center gap-2">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+    </svg>
+    Register
+  </a>
         <a href="#cargo" className="btn btn-primary btn-lg custom-btn">Login</a>
       </div>
     </div>
